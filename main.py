e7a56a1b0b2f049318d2ce2f0a90a6ca1d7bab62
Ahkahsiv <vishakha.yadav1901@gmail.com>
2023-02-04 17:32:26 +0530

    show_data

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from user import routes as AdminRoute
 from tortoise.contrib.fastapi import register_tortoise
 from user import api as apiroute
 from starlette.middleware.sessions import SessionMiddleware
-
+from starlette.middleware import Middleware
+from starlette.templating import Jinja2Templates
 
 app=FastAPI() 
 
@@ -14,6 +15,8 @@ app=FastAPI()
 
 db_url=DATABASE_URL()
 app.add_middleware(SessionMiddleware, secret_key="some-random-string", max_age=None)
+templates = Jinja2Templates(directory="templates")
+
 
 
 app.include_router(AdminRoute.router,tags=["Admin"])
--- user/routes.py
+++ user/routes.py
@@ -8,7 +8,7 @@ from passlib.context import CryptContext
 from fastapi_login import LoginManager
 import secrets
 import flash
-
+import typing
 
 
 router= APIRouter()
@@ -16,7 +16,18 @@ SECRET= 'your-secret-key'
 manager = LoginManager(SECRET, token_url='/auth/token')
 templates= Jinja2Templates ( directory = "user/templates" )
 
+def flash(request: Request, message: typing.Any, category: str = "primary") -> None:
+   if "_messages" not in request.session:
+       request.session["_messages"] = []
+       request.session["_messages"].append({"message": message, "category": category})
+
+def get_flashed_messages(request: Request):
+   print(request.session)
+   return request.session.pop("_messages") if "_messages" in request.session else []
+
+templates.env.globals['get_flashed_messages'] = get_flashed_messages
 pwd_context=CryptContext(schemes=["bcrypt"], deprecated ="auto")
+
 
 
 def get_password_hash(password):
@@ -41,15 +52,15 @@ async def create_user( request : Request , email:str = Form(...),
                         phone:str = Form(...),
                         password:str = Form(...)):
 
-    # if "_messages" in request.session:
-    #     print(request.session["_message"][0]["username"])
-    #     phone=request.session["_message"][0]["username"]
+    if "_messages" in request.session:
+        print(request.session["_message"][0]["phone"])
+        phone=request.session["_message"][0]["phone"]
     
-    # elif "_messages" in request.session:
-    #     print(request.session["_message"][0]["username"])
-    #     email=request.session["_message"][0]["username"]
+    elif "_messages" in request.session:
+        print(request.session["_message"][0]["email"])
+        email=request.session["_message"][0]["email"]
 
-    # else:
+    else:
         user_obj=await User.create(email=email,name=name,
                                      phone=phone
                                      ,password= get_password_hash(password))
@@ -71,19 +82,20 @@ async def login(request:Request, email:str =Form(...),
     
     email=email
     user = await load_user(email)
-
-    if not User:
-        # flash(request, 'User not exists', "danger")
-        return RedirectResponse("/login/", status_code=status.HTTP_302_FOUND)
+    
+    if not user:
+        flash(request, 'User not exists', "danger")
+        return templates.TemplateResponse("login.html", {'request':request})
     elif not verify_password(password, user.password):
-        # flash(request, 'Password Incorrect', "danger")
-        return templates.TemplateResponse("index.html", {'request':request})
+        flash(request, 'Password Incorrect', "danger")
+        return templates.TemplateResponse("login.html", {'request':request})
     else:
         # request.session['id'] = user.id
         request.session['name']= user.name
         print(user.name)
-
-        return templates.TemplateResponse("welcome.html", {'request':request})
+        flash(request, 'Login Successfull', "success")
+        data= await User.all()
+        return templates.TemplateResponse("welcome.html", {'request':request,'data':data})
 
     
 
@@ -96,3 +108,5 @@ async def login(request:Request, email:str =Form(...),
 @router.get("/welcome/", response_class=HTMLResponse)
 def read_item(request: Request):
     return templates.TemplateResponse("welcome.html", {"request": request})
+
+
